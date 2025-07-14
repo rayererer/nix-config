939ba668746b096b33e15a47d899388297b87cfd
rayererer <119081004+rayererer@users.noreply.github.com>
2025-07-15 01:35:28 +0200

    Removed unnecessary commented stuff from hyprland module

--- nixosModules/desktops/hyprland.nix
+++ nixosModules/desktops/hyprland.nix
@@ -50,24 +50,6 @@ in {
         environment.systemPackages = [
           pkgs.kitty # Required for the default Hyprland config
         ];
-
-        # xdg.portal = {
-        # enable = true;
-        # extraPortals = [
-        # pkgs.xdg-desktop-portal-hyprland # Probably necessary for stuff:
-        # # pkgs.xdg-desktop-portal-gtk # Keeping it here if necessary
-        # ];
-
-        # # Set default in case multiple backends are installed:
-        # config.common.default = "xdg-desktop-portal-hyprland";
-        # };
-
-        environment.sessionVariables = {
-          # Optional, hint Electron apps to use Wayland.
-          # NIXOS_OZONE_WL = "1";
-          # Optional, workaraound for GPU/driver issues with hardware cursors.
-          # WLR_NO_HARDWARE_CURSORS = "1";
-        };
       }
     ]
   );
