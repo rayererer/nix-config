a725588a163e9b42c26d83cf094149b96063e092
rayererer <119081004+rayererer@users.noreply.github.com>
2025-09-10 07:21:44 +0200

    Brought in testing file from the bundles branch and fixed it

---
+++ homeManagerModules/testing.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  config,
+  helpers,
+  ...
+}: let
+  cfg = config.my.testing;
+  bundleUtils = helpers.bundles.bundleUtils;
+in {
+  options.my = {
+    testing = {
+      enable = lib.mkEnableOption ''
+        Enable the testing module for whatever I need in the moment.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+    (bundleUtils.mkBundleConfig
+      {
+        # Bundle testing here.
+      })
+    {
+      # Normal config testing
+    }
+  ]);
+}
